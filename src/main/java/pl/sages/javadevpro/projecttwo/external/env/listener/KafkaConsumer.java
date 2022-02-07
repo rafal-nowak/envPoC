87a3f8149f185129dd58d84a91d66bd3be03a452
Marcin Kowalski <raggy2k4@gmail.com>
2022-02-07 19:01:47 +0100

    fixup sout log to @Log info

--- src/main/java/pl/sages/javadevpro/projecttwo/external/env/listener/KafkaConsumer.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/external/env/listener/KafkaConsumer.java
@@ -2,6 +2,7 @@ package pl.sages.javadevpro.projecttwo.external.env.listener;
 
 
 import lombok.AllArgsConstructor;
+import lombok.extern.java.Log;
 import org.springframework.kafka.annotation.KafkaListener;
 
 import org.springframework.kafka.core.KafkaTemplate;
@@ -10,7 +11,7 @@ import pl.sages.javadevpro.projecttwo.external.env.usertask.UserTaskStatusEnv;
 import pl.sages.javadevpro.projecttwo.external.env.executor.TaskExecutor;
 import pl.sages.javadevpro.projecttwo.external.env.usertask.UserTaskEnv;
 
-
+@Log
 @Service
 @AllArgsConstructor
 public class KafkaConsumer {
@@ -19,12 +20,11 @@ public class KafkaConsumer {
 
     private static final String TOPIC = "Kafka_Task_Report_json";
 
-
     @KafkaListener(topics = "Kafka_Task_json", groupId = "group_json",
             containerFactory = "taskKafkaListenerFactory")
     public void consumeJson(UserTaskEnv task) {
 
-        System.out.println("Consumed JSON Task: " + task);
+        log.info("Consumed JSON Task: " + task);
 
         TaskExecutor taskExecutor = new TaskExecutor(task);
         var result = taskExecutor.execute();
@@ -32,7 +32,6 @@ public class KafkaConsumer {
         task.setTaskStatus(result == 0 ? UserTaskStatusEnv.COMPLETED : UserTaskStatusEnv.FAILED);
 
         kafkaTemplate.send(TOPIC, task);
-        System.out.println("Finished JSON Task: " + task);
-
+        log.info("Finished JSON Task: " + task);
     }
 }
--- src/main/java/pl/sages/javadevpro/projecttwo/external/env/usertask/UserTaskEnv.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/external/env/usertask/UserTaskEnv.java
@@ -9,16 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserTaskEnv {
 
-//    private String taskId;
-//    private String userId;
-//    private String taskPath;
-//    private String taskStatus;
-
     private String id;
     private String name;
     private String description;
     private String userTaskFolder;
     private UserTaskStatusEnv taskStatus;
     private String userEmail;
-
 }
