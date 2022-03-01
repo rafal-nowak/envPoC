5f54ae53c3a82ef278ec2abf1b0a866acd0c6f22
rafal-nowak <contact@rafal-nowak.com>
2022-03-01 14:14:37 +0100

    refactor

---
+++ src/main/java/pl/sages/javadevpro/projecttwo/external/env/executor/TaskExecutorStatus.java
@@ -0,0 +1,7 @@
+package pl.sages.javadevpro.projecttwo.external.env.executor;
+
+public enum TaskExecutorStatus {
+
+    COMPLETED, FAILED
+
+}
--- src/main/java/pl/sages/javadevpro/projecttwo/external/env/listener/KafkaService.java
+++ src/main/java/pl/sages/javadevpro/projecttwo/external/env/listener/KafkaService.java
@@ -7,20 +7,24 @@ import org.springframework.kafka.annotation.KafkaListener;
 
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import pl.sages.javadevpro.projecttwo.external.env.executor.TaskExecutorStatus;
 import pl.sages.javadevpro.projecttwo.external.env.usertask.UserTaskStatusEnv;
 import pl.sages.javadevpro.projecttwo.external.env.executor.TaskExecutor;
 import pl.sages.javadevpro.projecttwo.external.env.usertask.UserTaskEnv;
 
+import static pl.sages.javadevpro.projecttwo.external.env.config.KafkaConfiguration.KAFKA_GROUP_ID;
+import static pl.sages.javadevpro.projecttwo.external.env.config.KafkaConfiguration.KAFKA_LISTENER_TOPIC;
+import static pl.sages.javadevpro.projecttwo.external.env.config.KafkaConfiguration.KAFKA_PRODUCER_TOPIC;
+
 @Log
 @Service
 @AllArgsConstructor
-public class KafkaConsumer {
+public class KafkaService {
 
     private KafkaTemplate<String, UserTaskEnv> kafkaTemplate;
 
-    private static final String TOPIC = "Kafka_Task_Report_json";
 
-    @KafkaListener(topics = "Kafka_Task_json", groupId = "group_json",
+    @KafkaListener(topics = KAFKA_LISTENER_TOPIC, groupId = KAFKA_GROUP_ID,
             containerFactory = "taskKafkaListenerFactory")
     public void consumeJson(UserTaskEnv task) {
 
@@ -29,9 +33,9 @@ public class KafkaConsumer {
         TaskExecutor taskExecutor = new TaskExecutor(task);
         var result = taskExecutor.execute();
 
-        task.setTaskStatus(result == 0 ? UserTaskStatusEnv.COMPLETED : UserTaskStatusEnv.FAILED);
+        task.setTaskStatus(result == TaskExecutorStatus.COMPLETED ? UserTaskStatusEnv.COMPLETED : UserTaskStatusEnv.FAILED);
 
-        kafkaTemplate.send(TOPIC, task);
+        kafkaTemplate.send(KAFKA_PRODUCER_TOPIC, task);
         log.info("Finished JSON Task: " + task);
     }
 }
