ec409a7c52ce785b4b3f2277101a0f091e27cfa7
rafal-nowak <contact@rafal-nowak.com>
2022-03-03 10:41:31 +0100

    introduced TaskExecutor interface

---
+++ src/main/java/pl/sages/javadevpro/projecttwo/external/env/executor/TaskExecutorService.java
@@ -0,0 +1,29 @@
+package pl.sages.javadevpro.projecttwo.external.env.executor;
+
+import lombok.AllArgsConstructor;
+import pl.sages.javadevpro.projecttwo.external.env.usertask.UserTaskEnv;
+
+import java.io.IOException;
+
+@AllArgsConstructor
+public class TaskExecutorService implements TaskExecutor{
+
+    private UserTaskEnv task;
+
+    @Override
+    public TaskExecutorStatus execute() {
+
+        try {
+            String commands = "cd " + task.getUserTaskFolder() + "; docker-compose up";
+            Process p = Runtime.getRuntime().exec(new String[]{"bash", "-c", commands});
+            int status = p.waitFor();
+            if (status == 0)
+                return TaskExecutorStatus.COMPLETED;
+            return TaskExecutorStatus.FAILED;
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+            return TaskExecutorStatus.FAILED;
+        }
+
+    }
+}
